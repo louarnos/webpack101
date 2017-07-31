8744a1391de347990e16275462b155e66c4fc15a
louarnos <louarnos@gmail.com>
2017-07-30 20:22:26 -0400

    Use correct webpack2 syntax for loaders

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
 	module: {
 		rules: [
-			{ test: /\.css$/, loaders: 'style-loader!css-loader' }
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] }
 		]
 	},
 	plugins: [
